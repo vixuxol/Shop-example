e544c1c771217b185fa8d197c34fd0d1548ba559
Irina Belova <i.m.belova@mail.ru>
2022-11-16 10:39:17 +0300

    Добавлены основные стили

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Container } from 'react-bootstrap';
+import { HomeScreen } from './screens/HomeScreen';
+import { ProductScreen } from './screens/ProductScreen';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className = "py-3">
+          <Container>
+            <Routes>
+              <Route path='/' element = {<HomeScreen />} />
+              <Route path='/product/:id' element = {<ProductScreen />} />
+            </Routes>
+          </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
