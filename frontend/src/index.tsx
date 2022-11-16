dbe3069f9ae9a81752d308e9e7801d5d86f41100
Irina Belova <i.m.belova@mail.ru>
2022-11-16 11:15:30 +0300

    Добавлен Reduxstore

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,12 +5,17 @@ import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
