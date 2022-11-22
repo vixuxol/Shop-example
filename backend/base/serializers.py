5fd1f74b67b8930b78ac19c7ac54d8c1c561f3d5
Irina Belova <i.m.belova@mail.ru>
2022-11-22 13:21:24 +0300

    Добавлена аутентификация по токенам

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,3 +10,59 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # fields = ['_id', 'name', 'image', 'description', 'brand', 'category', 'price', 'countInStock', 'rating', 'numReviews']
 
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only = True)
+    _id = serializers.SerializerMethodField(read_only = True)
+    isAdmin = serializers.SerializerMethodField(read_only = True)
+
+    class Meta:
+        model = User
+        fields = ['_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    #чтобы вернуть тип имя (все храним в поле first_name)   
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = User
+        fields = ['_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    # если хотим поменять ответ ручки
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # data['username'] = self.user.username
+        # data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        return data
+    # если хотим поменять внутри самого токена
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+
+    #     # Add custom claims
+    #     token['username'] = user.username
+    #     token['message'] = 'hello world'
+    #     # ...
+
+    #     return token
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .models import Product
-from .serializers import ProductSerializer
+from django.contrib.auth.models import User
+from .serializers import ProductSerializer, UserSerializer
+
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import MyTokenObtainPairSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
 
@@ -38,4 +48,20 @@ def getProducts(request):
 def getProduct(request, pk):
     product = Product.objects.get(_id = pk)
     serializer = ProductSerializer(product, many = False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user # если пользователь залогинился с помощью системы по умолчанию - 
+                        # его можно увиедть таким образом
+   
+    serializer =UserSerializer(user, many = False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many = True)
     return Response(serializer.data)
